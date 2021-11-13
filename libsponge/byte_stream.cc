dab6ce079a17cb50d5c96897b29edf7e27842710
tigertang <tigertang@zju.edu.cn>
2021-11-13 20:20:43 +0800

    Fix lab0 bugs

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -9,27 +9,29 @@
 
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) {
-    _buffer.resize(capacity);
+ByteStream::ByteStream(const size_t capacity): _buffer(capacity, ' ') {
     _len = 0;
 }
 
 size_t ByteStream::write(const string &data) {
     uint32_t ret = std::min(data.size(), remaining_capacity());
-    for (int i = 0; i < ret; i++) {
+    for (uint32_t i = 0; i < ret; i++) {
         _buffer[i + _len] = data[i];
     }
     _len += ret;
-    _bytes_written += data.size();
+    _bytes_written += ret;
     return ret;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
-string ByteStream::peek_output(const size_t len) const { return _buffer.substr(0, len); }
+string ByteStream::peek_output(const size_t len) const {
+    return _buffer.substr(0, len);
+}
 
 //! \param[in] len bytes will be removed from the output side of the buffer
 void ByteStream::pop_output(const size_t len) {
-    for (int i = 0; i < _len - len; i++) {
+    _bytes_read += len;
+    for (uint32_t i = 0; i < _len - len; i++) {
         _buffer[i] = _buffer[i + len];
     }
     _len -= len;
@@ -41,7 +43,6 @@ void ByteStream::pop_output(const size_t len) {
 std::string ByteStream::read(const size_t len) {
     auto s = peek_output(len);
     pop_output(len);
-    _bytes_read += len;
     return s;
 }
 
--- libsponge/byte_stream.hh
+++ libsponge/byte_stream.hh
@@ -17,7 +17,8 @@ class ByteStream {
     // that's a sign that you probably want to keep exploring
     // different approaches.
     std::string _buffer;
-    uint32_t _len, _bytes_written = 0, _bytes_read = 0;
+    uint32_t _len;
+    mutable uint32_t _bytes_written = 0, _bytes_read = 0;
     bool _input_ended = false;
     bool _error = false;  //!< Flag indicating that the stream suffered an error.
 
