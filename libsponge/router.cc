ec58a1bca49cd2bfb4f25b22d9c842271826ae5d
tigertang <tigertang.zju@gmail.com>
2022-01-03 23:36:57 +0800

    Add Lab6 (#7)
    * CS144 Lab checkpoint 1 starter code
    
    * CS144 Lab checkpoint 2 starter code
    
    * CS144 Lab checkpoint 3 starter code
    
    * CS144 Lab checkpoint 4 starter code
    
    * CS144 Lab checkpoint 5 starter code
    
    * CS144 Lab checkpoint 6 starter code
    
    * Add router
    
    * Pass router tests
    
    Co-authored-by: Keith Winstein <keithw@cs.stanford.edu>
    Co-authored-by: heyshb <heyshb@vip.qq.com>

---
+++ libsponge/router.cc
@@ -0,0 +1,72 @@
+#include "router.hh"
+
+#include <iostream>
+
+using namespace std;
+
+// Dummy implementation of an IP router
+
+// Given an incoming Internet datagram, the router decides
+// (1) which interface to send it out on, and
+// (2) what next hop address to send it to.
+
+// For Lab 6, please replace with a real implementation that passes the
+// automated checks run by `make check_lab6`.
+
+// You will need to add private members to the class declaration in `router.hh`
+
+//! \param[in] route_prefix The "up-to-32-bit" IPv4 address prefix to match the datagram's destination address against
+//! \param[in] prefix_length For this route to be applicable, how many high-order (most-significant) bits of the route_prefix will need to match the corresponding bits of the datagram's destination address?
+//! \param[in] next_hop The IP address of the next hop. Will be empty if the network is directly attached to the router (in which case, the next hop address should be the datagram's final destination).
+//! \param[in] interface_num The index of the interface to send the datagram out on.
+void Router::add_route(const uint32_t route_prefix,
+                       const uint8_t prefix_length,
+                       const optional<Address> next_hop,
+                       const size_t interface_num) {
+    cerr << "DEBUG: adding route " << Address::from_ipv4_numeric(route_prefix).ip() << "/" << int(prefix_length)
+         << " => " << (next_hop.has_value() ? next_hop->ip() : "(direct)") << " on interface " << interface_num << "\n";
+    _routes.emplace_back(route_prefix, prefix_length, next_hop, interface_num);
+}
+
+//! \param[in] dgram The datagram to be routed
+void Router::route_one_datagram(InternetDatagram &dgram) {
+    optional<uint32_t> j;
+    for (uint32_t i = 0; i < _routes.size(); i++) {
+        uint32_t route_prefix;
+        uint8_t prefix_length;
+        tie(route_prefix, prefix_length, ignore, ignore) = _routes[i];
+
+        uint32_t mask_length = 32 - prefix_length;
+        auto dgram_dst_masked = static_cast<uint64_t>(dgram.header().dst) >> mask_length;
+        auto route_prefix_masked = static_cast<uint64_t>(route_prefix) >> mask_length;
+        if (dgram_dst_masked != route_prefix_masked) {
+            continue;
+        }
+        if (j == nullopt || prefix_length > get<1>(_routes[j.value()])) {
+            j = i;
+        }
+    }
+
+    if (j == nullopt)
+        return;
+    // drops the datagram
+
+    if (dgram.header().ttl <= 1)
+        return;
+    dgram.header().ttl -= 1;
+
+    Address next_hop = get<2>(_routes[j.value()]) == nullopt ? Address::from_ipv4_numeric(dgram.header().dst)
+                                                             : get<2>(_routes[j.value()]).value();
+    _interfaces[get<3>(_routes[j.value()])].send_datagram(dgram, next_hop);
+}
+
+void Router::route() {
+    // Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
+    for (auto &interface : _interfaces) {
+        auto &queue = interface.datagrams_out();
+        while (not queue.empty()) {
+            route_one_datagram(queue.front());
+            queue.pop();
+        }
+    }
+}
